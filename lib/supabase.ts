eb81843b95f4f0ef8e646dc8ecf49f36e35fd781
ListyA6 <zhugelisty@gmail.com>
2026-03-02 15:03:57 +0700

    Update env variable names for Vercel

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_RESTAURANT!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_RESTAURANT!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
